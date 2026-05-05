254c22c6ce7847f99bdc77727299b0d3acd94e37
thaissiani <sianithais@gmail.com>
2026-05-05 16:05:03 -0300

    Adiciona arquivo produto.hpp

---
+++ include/produto.hpp
@@ -0,0 +1,51 @@
+#ifndef PRODUTO_HPP
+#define PRODUTO_HPP
+
+#include <string>
+class estoque;
+
+/**
+ * @brief Armazena e fornece as informações de um livro.
+ */
+
+class produto {
+
+private:
+
+    int id;
+    std::string nome;
+    std::string descricao;
+    float preco;
+    int quantidadeEstoque;
+
+public:
+
+    produto(int id, std::string nome, std::string descricao, float preco, int quantidadeEstoque);
+
+/**
+* @brief Retorna o identificador único do livro. 
+*/
+    int getId();
+
+/**
+* @brief Retorna o nome do livro.
+*/
+    std::string getNome();
+
+/**
+ * @brief Retorna a descrição do livro.
+ */
+    std::string getDescricao();
+
+/**
+* @brief Retorna o preço do livro.
+*/
+    float getPreco();
+
+/**
+* @brief Retorna a quantidade atual de produtos em estoque.
+*/
+    int getQuantidadeEstoque();
+};
+
+#endif
