67af1d1ec31971c32c7fa6842f5d196cccee766e
djuliasouza <djuliakelly6@gmail.com>
2026-05-06 20:56:18 -0300

    Carrinho.hpp revisado

--- include/Carrinho.hpp
+++ include/Carrinho.hpp
@@ -2,70 +2,99 @@
 #define CARRINHO_HPP
 #include <vector>
 
-class Produto; 
+class Produto;
 class Cliente;
 
 /**  
- * @brief Classe Carrinho representa um carrinho de compras.
- * 
- * Esta classe é responsável por armazenar os itens adicionados ao carrinho, calcular o total da compra e fornecer métodos para manipular os itens.
+ * @class Carrinho 
+ * @brief Representa o carrinho ativo de um cliente durante a navegação do e - commerce.
  */
 
 class Carrinho {
     private:
-    std::vector<Produto*> _produtos; 
+    std::vector<Produto> _produtos; 
     std::vector<int> _quantidades; 
-    Cliente* _cliente; 
+    Cliente& _cliente; 
     double _valorFrete; 
     
     public:
-    /** @brief Construtor da classe Carrinho.
-     * @param cliente Ponteiro para o cliente associado ao carrinho.
+    /** 
+     * @brief Cria um carrinho associado a um cliente específico.
      */
-    Carrinho(Cliente* cliente);
+    Carrinho(Cliente& cliente);
 
-    /** @brief Adiciona um produto ao carrinho.
-     * @param produto Ponteiro para o produto a ser adicionado.
+    /**
+     * @brief Busca o índice de um produto no carrinho com base no ID do produto.
+     * Esse metodo é utilizado para evitar a duplicação de produtos no carrinho;
+     * Todas as operações de modificação dependem dessa busca.
+     */
+    int buscarIdProduto(int idProduto) const;
+
+    /** 
+     * @brief Adiciona um produto ao carrinho ou incrementa a sua quantidade.
+     * @param produto produto a ser adicionado.
      * @param quantidade Quantidade do produto a ser adicionada.
      */
-    void adicionarProduto(Produto* produto, int quantidade);
+    void adicionarProduto(const Produto& produto, int quantidade);
 
-    /** @brief Remove um produto do carrinho.
+    /** 
+     * @brief Remove um produto do carrinho.
      * @param idProduto ID do produto a ser removido.
      */
     void removerProduto(int idProduto);
 
-    /** @brief Atualiza a quantidade de um produto no carrinho.
+    /**
+     * @brief Atualiza a quantidade de um produto no carrinho.
      * @param idProduto ID do produto a ser atualizado.
      * @param novaQuantidade Nova quantidade do produto.
      */
     void atualizarQuantidade(int idProduto, int novaQuantidade);
 
-    /**@brief Verifica se há disponibilidade de estoque para todos os produtos no carrinho.
-     * @param produto Ponteiro para o produto a ser verificado.
+    /**
+     * @brief Verifica se há disponibilidade de estoque para todos os produtos no carrinho.
+     * @param produto O produto a ser verificado.
      * @param quantidade Quantidade do produto a ser verificada.
      * @return Retorna true se todos os produtos estiverem disponíveis, false caso contrário.
      */
-    bool validarDisponibilidade(Produto* produto, int quantidade);
+    bool validarEstoque(const Produto& produto, int quantidade);
     
-    /** @brief Esvazia o carrinho, removendo todos os produtos e resetando o valor total da compra.
+    /** 
+     * @brief Esvazia o carrinho, removendo todos os produtos e resetando o valor total da compra.
      */
     void limparCarrinho();
     
-    /** @brief Calcula o subtotal dos produtos do carrinho.
+    /** 
+     * @brief Calcula o subtotal dos produtos do carrinho.
      * @return Retorna o valor do subtotal dos produtos no carrinho.
      */
     double calcularSubtotal() const;
 
-    /** @brief Calcula o valor total da compra, incluindo o subtotal dos produtos e o valor do frete.
+    /** 
+     * @brief Calcula o valor total da compra, incluindo o subtotal dos produtos e o valor do frete.
      * @return Retorna o valor total da compra.
      */
     double calcularTotal() const;
 
-    /** @brief Obtém o valor do frete para a compra.
-     * @return Retorna o valor do frete.
+    /** 
+     * @brief Calcula o valor do frete com base na localização do cliente.
+     * @return Retorna o valor do frete para a compra.
+     */
+    double calcularValorFrete() const;
+
+    /** 
+     * @brief Obtém o valor do frete para a compra.
      */
     double getValorFrete() const;
+      
+    /** 
+    * @brief Fornece acesso, somente para leitura, à lista de produtos no carrinho.
+    */
+    const std::vector<Produto>& getProdutos() const;
+
+    /** 
+    * @brief Fornece acesso, somente para leitura, à lista de quantidades dos produtos no carrinho.
+    */
+    const std::vector<int>& getQuantidades() const;
 
 };
  
