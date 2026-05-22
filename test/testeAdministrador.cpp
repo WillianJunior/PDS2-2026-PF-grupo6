6f2a8382b58493c7a2a5469401a8abeb3c778ffb
thaissiani <sianithais@gmail.com>
2026-05-22 14:49:44 -0300

    Update testeAdministrador.cpp

--- test/testeAdministrador.cpp
+++ test/testeAdministrador.cpp
@@ -1,19 +1,19 @@
 #include "doctest.h"
 #include <string>
 
-#include "Usuario.hpp"
-#include "Administrador.hpp"
-#include "Catalogo.hpp"
-#include "Cliente.hpp"
-#include "Pedido.hpp"
-#include "Carrinho.hpp"
+#include "../include/Administrador.hpp"
+#include "../include/Catalogo.hpp"
+#include "../include/Estoque.hpp"
+#include "../include/Cliente.hpp"
+#include "../include/Carrinho.hpp"
+#include "../include/Pedido.hpp"
 
 TEST_CASE("Construtor do Administrador") {
     Administrador admin("Root", "admin@loja.com", "123456");
 
-    CHECK(admin.validarEmail("admin@loja.com") == true);
-    CHECK(admin.validarEmail("email_invalido.com") == false);
-    CHECK(admin.permitirAlteracaoDados() == true);
+   CHECK(admin.getNome() == "Root");
+    CHECK(admin.getEmail() == "admin@loja.com");
+    CHECK(admin.getSenha() == "123456");
 }
 
 TEST_CASE("Gerenciar catalogo nao deve lançar excecao") {
@@ -32,14 +32,16 @@ TEST_CASE("Gerenciar estoque nao deve lançar excecao") {
 
 TEST_CASE("Gerenciar contas nao deve lançar excecao") {
     Administrador admin("Root", "admin@loja.com", "123456");
-    Cliente cli;
+    Cliente cli("Ana", "ana@email.com", "senha123", "52998224725");
 
     CHECK_NOTHROW(admin.gerenciarContas(cli));
 }
 
 TEST_CASE("Atualizar vendas nao deve lançar excecao") {
     Administrador admin("Root", "admin@loja.com", "123456");
-    Pedido p;
+    Cliente cli("Ana", "ana@email.com", "senha123", "52998224725");
+    Carrinho car(cli);
+    Pedido p(car, cli);
 
     CHECK_NOTHROW(admin.atualizarVendas(p));
 }
